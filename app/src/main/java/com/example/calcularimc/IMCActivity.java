ec2ab056de1433b9d943ce7b48b5058f260833af
moises.274288 <moises_sousa16@hotmail.com>
2020-10-02 15:54:51 -0300

    Calculo do imc

--- app/src/main/java/com/example/calcularimc/IMCActivity.java
+++ app/src/main/java/com/example/calcularimc/IMCActivity.java
@@ -1,4 +1,5 @@
 package com.example.calcularimc;
+import java.text.DecimalFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,18 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class IMCActivity extends AppCompatActivity {
 
-    private TextView IMC;
+    private TextView imc;
     private Button button;
     private TextView resultado;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_i_m_c);
 
+        imc = findViewById(R.id.ResultadoTextView);
+        button = findViewById(R.id.RecalcularIMCButton);
+        resultado = findViewById(R.id.mostrandoIMC);
 
+        Intent intent = getIntent();
+        double ResultadoIMC1 = intent.getDoubleExtra("imc",0);
+        imc.setText(new DecimalFormat("##.##").format(ResultadoIMC1));
 
     button=findViewById(R.id.RecalcularIMCButton);
     button.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +38,8 @@ public class IMCActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(IMCActivity.this,MainActivity.class);
             startActivity(intent);
+
+
 
         }
     });
--- app/src/main/java/com/example/calcularimc/MainActivity.java
+++ app/src/main/java/com/example/calcularimc/MainActivity.java
@@ -21,41 +21,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        peso = findViewById(R.id.PesoEditText);
+        peso = findViewById(R.id.pesoEditText);
         altura = findViewById(R.id.alturaEditText);
         button = findViewById(R.id.calcularButton);
 
-        TextView pesoEditText = (TextView) findViewById(R.id.PesoEditText);
-        TextView alturaEditText = (TextView) findViewById(R.id.alturaEditText);
-        final TextView resutado = (TextView) findViewById(R.id.ResultadoTextView);
+
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, IMCActivity.class);
-                startActivity(intent);
-
                 String pesoString = peso.getText().toString();
                 double pesodouble = Double.parseDouble(pesoString);
 
                 String alturaString = altura.getText().toString();
                 double alturadouble = Double.parseDouble(alturaString);
 
-                String resultado = resutado.getText().toString();
-                double resultadodouble = Double.parseDouble(resultado);
+                double imc= calculaIMC(pesodouble,alturadouble);
 
-                resultadodouble = pesodouble /(alturadouble*alturadouble);
+                Intent intent = new Intent(MainActivity.this, IMCActivity.class);
+                intent.putExtra("imc",imc);
+                startActivity(intent);
+
 
             }
         });
+
     }
-    private void enviaResultado(double resultadodouble){
-        Intent intent = new Intent(MainActivity.this,IMCActivity.class);
-        startActivity(intent);
+   private double calculaIMC(double pesodouble, double alturadouble){
+        return pesodouble/ (alturadouble*alturadouble);
+
     }
 
-};
+}
+
+
 
 
